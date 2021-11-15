992fd7fd130ac66d898b2bce093569683e624640
Vhalia <71339069+Vhalia@users.noreply.github.com>
2021-11-15 09:08:59 +0100

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 		<section id="contenu">
 			<h2>Accueil</h2>
-			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
+			<p>Bienvenue à la séance 7 d'infra où le but est de créer une app heroku!</p>
 			<p>Jean-Luc Collinet.</p>
 		</section>
